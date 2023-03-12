db0b715ad242f2e4044acf9fa7e6bed56bb049c1
Manan <mananshah256462@gmail.com>
2023-03-12 12:48:35 +0530

    Please Add twice

--- src/test/java/DevClass.java
+++ src/test/java/DevClass.java
@@ -3,5 +3,6 @@ public class DevClass {
             System.out.println("Hello world!");
             System.out.println("Hello world2!");
             System.out.println("Hello world4!");
+            //Manan Added this
         }
 }
